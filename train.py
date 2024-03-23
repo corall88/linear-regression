6d7dce6527fc207ea19252be6b1342330b826d7f
corall88 <163779022+corall88@users.noreply.github.com>
2024-03-23 18:53:16 +0300

    Update train.py

--- train.py
+++ train.py
@@ -1,16 +1,16 @@
 import numpy as np
 import pandas as pd
 from keras.models import Sequential
-from keras.layers import Dense
-from keras.optimizers import Nadam
+from keras.layers import Dense, Dropout
+from keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
-from keras.regularizers import l1
+from keras.regularizers import l1_l2
 from tensorflow.keras.callbacks import ModelCheckpoint
 
-EPOCHS = 400
-BATCH_SIZE = 180
-LEARNING_RATE = 0.003
+EPOCHS = 180
+BATCH_SIZE = 50
+LEARNING_RATE = 0.0003
 FILENAME = "train_merged.csv"
 
 def prepare_data(filename):
@@ -18,21 +18,20 @@ def prepare_data(filename):
     n_features = len(df_train.columns) - 2
     X_train = df_train.iloc[:, 2:]
     y_train = df_train.iloc[:, 1]
-    X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.2, random_state=23)
+    X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
     
     return X_train, y_train, X_test, y_test, n_features
 
 def build_model(n_features, learning_rate):
     model = Sequential([
-        Dense(75, input_dim=n_features, activation='relu', kernel_regularizer=l1(0.00001)),
-        Dense(30, activation='selu', kernel_regularizer=l1(0.00001)),
-        Dense(15, activation='relu', kernel_regularizer=l1(0.00001)),
-        Dense(8, activation='relu'),
-        Dense(4, activation='relu'), 
-        Dense(1, activation='linear')
-    ])
-
-    model.compile(optimizer=Nadam(learning_rate=learning_rate), loss='mean_absolute_error')
+    Dense(512, input_dim=n_features, activation='relu'),
+    Dense(256, activation='relu'),
+    Dense(128, activation='relu'),
+    Dense(64, activation='relu'),
+    Dense(32, activation='relu'),
+    Dense(1, activation='linear')
+])
+    model.compile(optimizer=Adam(learning_rate=learning_rate), loss='mean_absolute_error')
 
     return model
 
@@ -50,5 +49,4 @@ checkpoint_callback = ModelCheckpoint(filepath='best_model.h5',
                                       save_best_only=True,
                                       verbose=0)
 
-model.fit(x=X_train, y=y_train, epochs=EPOCHS, batch_size=BATCH_SIZE, verbose=1, validation_data=(X_test, y_test), callbacks=[checkpoint_callback])
-model.save('model.h5')
+history = model.fit(x=X_train, y=y_train, epochs=EPOCHS, batch_size=BATCH_SIZE, verbose=1, validation_data=(X_test, y_test), callbacks=[checkpoint_callback])
